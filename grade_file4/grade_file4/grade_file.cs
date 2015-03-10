e62fa33016a02bcee4b68d50d4dcdd249a6cd116
Rhernandez513 <rhernandez513@gmail.com>
2015-03-10 13:12:37 -0500

    string sample to categoryFileName

--- grade_file4/grade_file4/grade_file.cs
+++ grade_file4/grade_file4/grade_file.cs
@@ -9,27 +9,27 @@ namespace IntroCS
 		public static void Main(string[]args)
 		{
 			{
-				string sample = "sample.txt";
+				string categoryFileName = "sample.txt";
 				string output = "output.txt";
 				Console.WriteLine("Directory of {0}: {1}",
-					sample, FIO.GetLocation(sample));
+					categoryFileName, FIO.GetLocation(categoryFileName));
 				Console.WriteLine("Path to {0}: {1}",
-					sample, FIO.GetPath(sample));
-				StreamReader reader1 = FIO.OpenReader(sample);
+					categoryFileName, FIO.GetPath(categoryFileName));
+				StreamReader reader1 = FIO.OpenReader(categoryFileName);
 				if (reader1 != null) {
 					Console.Write(reader1.ReadToEnd());
 					Console.WriteLine("First reader test passed.");
 					reader1.Close();
 				}
-				StreamReader reader2 = FIO.OpenReader(FIO.GetLocation(sample),
-					sample);
+				StreamReader reader2 = FIO.OpenReader(FIO.GetLocation(categoryFileName),
+					categoryFileName);
 				if (reader2 != null) {
 					Console.WriteLine("Second reader test passed.");
 					reader2.Close();
 				}
-				StreamWriter writer1 = FIO.OpenWriter(FIO.GetLocation(sample),
+				StreamWriter writer1 = FIO.OpenWriter(FIO.GetLocation(categoryFileName),
 					output);
-				writer1.WriteLine("File in the same directory as {0}.", sample);
+				writer1.WriteLine("File in the same directory as {0}.", categoryFileName);
 				writer1.Close();
 				Console.WriteLine("Writer test passed; file written at \n {0}",
 					FIO.GetPath(output));
